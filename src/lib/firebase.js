14ec3ed05739cf4e48935e9708cd2789df15c737
EC2 Default User <kun_duc123@yahoo.com.vn>
2021-05-21 08:44:50 +0000

    fix bug

---
+++ src/lib/firebase.js
@@ -0,0 +1,57 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1LPoJP1eQtoh2ZBU0Re0cZvbv_bXzaYw",
+    authDomain: "fir-sample-8ba40.firebaseapp.com",
+    projectId: "fir-sample-8ba40",
+    storageBucket: "fir-sample-8ba40.appspot.com",
+    messagingSenderId: "344025792942",
+    appId: "1:344025792942:web:990aca55cec793d937083f"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+export const auth = firebase.auth();
+export default firebase;
+
+export const getFirebaseItems = async () => {
+  try {
+    const snapshot = await db
+      .collection("todos")
+      .get();
+    const items = snapshot.docs.map(
+      (doc) => ({ ...doc.data(), id: doc.id })
+    );
+    return items;
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
+
+export const addFirebaseItem = async (item) => {
+  try {
+    const todoRef = db.collection("todos");
+    await todoRef.add(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const updateFirebaseItem = async (item, id) => {
+  try {
+    const todoRef = db.collection("todos").doc(id);
+    await todoRef.update(item);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const clearFirebaseItem = async (item) => {
+  const todoRef = db.collection("todos").doc(item.id);
+  await todoRef.delete().then(function () {
+  }).catch(function (err) {
+    console.log(err);
+  });
+};
